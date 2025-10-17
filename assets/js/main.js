10bbb5be268466009045a9d3fa9ade36941e14e6
erikagdd <eriguided@alu.edu.gva.es>
2025-10-17 12:34:34 +0200

    Usuari2: he afegit una secció per modificar atributs HTML amb setAttribute()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-// Fitxer JavaScript buit - s'omplirà en les següents features
+// Funció per a modificar el contingut d'un element HTML
+function canviaText() {
+  document.getElementById("textExample").innerHTML =
+    "El text ha sigut modificat correctament!";
+}
+
+function canviaImatge() {
+  let imatge = document.getElementById("myImage");
+  imatge.setAttribute("src", "assets/img2.png");
+  imatge.setAttribute("alt", "Imatge modificada amb JavaScript");
+}
